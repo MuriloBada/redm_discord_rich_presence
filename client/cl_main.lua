8d460d2b6e3270e6bc21e05626873b3fb7b1e635
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-02-09 20:12:08 -0300

    commit

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,26 +1,53 @@
--- client
+RedEM = exports["redem_roleplay"]:RedEM()
 local gNumPlayers = 1
 
 local gLastRichPresenceUpdateTime
 
-local function UpdateRichPresence()
+local function UpdateRichPresence(source)
+    local Player = RedEM.GetPlayer(source) 
+    if Player then
+        local PlayerName = Player.firstname..' '..Player.lastname
+        local gameTimer = GetGameTimer()
 
-    local gameTimer = GetGameTimer()
+        if not gLastRichPresenceUpdateTime or gameTimer - gLastRichPresenceUpdateTime >= 30000 then
 
-    if not gLastRichPresenceUpdateTime or gameTimer - gLastRichPresenceUpdateTime >= 30000 then
+            gLastRichPresenceUpdateTime = gameTimer
 
-        gLastRichPresenceUpdateTime = gameTimer
+            -- This is the Application ID (Replace this with you own)
+            SetDiscordAppId(966363661167911012)
+            -- SetDiscordAppId(YourApplicationIDHere)
+            
+            SetRichPresence(PlayerName.." ["..source.."]\nRedM Server "..gNumPlayers..'/128') -- This will take the player name and the source and the current amount of online players
+            
+            -- Here you will have to put the image name for the "large" icon.
+            -- You can create one by go to Rich Presence/Art Assets tab in your application and  click Add Image(s)
+            -- The Paramater is your Image key that you uploaded (you can change it too once you upload)
+            SetDiscordRichPresenceAsset('derp')
+            -- SetDiscordRichPresenceAsset('LargeIcon')
+            
 
-        SetDiscordAppId(966363661167911012) -- 
-        SetDiscordRichPresenceAsset('derp')
-        SetDiscordRichPresenceAssetText('Delaware Roleplay')
-        SetDiscordRichPresenceAssetSmall('info')
-        SetDiscordRichPresenceAssetSmallText('Venha Jogar com a gente!')
-        SetRichPresence("Habitantes: "..gNumPlayers.."/64\nDelaware Roleplay")
-        -- Actions    
-        SetDiscordRichPresenceAction(0, "DISCORD", "https://discord.gg/Gc2yRKr6ju")
-        SetDiscordRichPresenceAction(1, "JOGAR", "https://cfx.re/join/589dmr")
-        
+            -- Here you can add hover text for the "large" icon.
+            SetDiscordRichPresenceAssetText('RedEM:RP')
+            
+
+
+            -- Here is the same as the LargeIcon one, but for the Small one
+            SetDiscordRichPresenceAssetSmall('info')
+            
+            
+            -- Here you can add hover text for the "small" icon.
+            SetDiscordRichPresenceAssetSmallText('Come play with us')
+
+            --[[ 
+                Here you can add buttons that will display in your Discord Status,
+                First paramater is the button index (0 or 1), second is the title and 
+                last is the url (this has to start with "fivem://connect/" or "https://") 
+            ]]--
+            SetDiscordRichPresenceAction(0, "Join", "fivem://connect/YourIpHere")
+
+            --You can add more Natives Here vvv
+            --SetDiscordRichPresenceAction(1, "Example", "https://example.com")
+        end
     end
 end
 
@@ -32,7 +59,8 @@ CreateThread(function()
 end)
 
 RegisterNetEvent('discordRichPresenceSetNumPlayers', function(numPlayers)
+    local _source = source
     gNumPlayers = numPlayers
 
-    UpdateRichPresence()
+    UpdateRichPresence(_source)
 end)
--- readme.md
+++ readme.md
@@ -1,17 +1,8 @@
 # Discord Rich Presence
 
 ## 1. How to use it
-```Clone``` this repository from github and place inside your ```resources``` folder 
+```Clone``` this repository from github and place inside your ```resources``` folder<br>
 add ```start redemrp_discord_rich_presence``` in your ```server.cfg```
-
-### 2. Documentation
-
-You will need to adapt this resource for your informations, here has the information of what function does what.
-
-Attempt | 
----     | --- |
-Seconds | 301 |
-
 
 # Credits
 [Murilo "KIFOO" Bada](https://github.com/MuriloBada)
